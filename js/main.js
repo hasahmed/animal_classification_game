04669abe15116f2fb3d773cf052966649f7685df
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-12-06 21:01:01 -0500

    made a lot of nice progress

--- js/main.js
+++ js/main.js
@@ -1,44 +1,52 @@
-const soundMessage = new Audio('media/msg.mp3');
-const animalClasses = [
-    "amphibians",
-    "arthropods",
-    "birds",
-    "fish",
-    "mammals",
-    "reptiles"
-];
-const mammals = [
-    "dog",
-    "cow",
-    "cat"
-];
+Object.defineProperty(String.prototype, 'capitilize', {
+    value: function(){
+        return this.charAt(0).toUpperCase() + this.substring(1)
+    }
+});
+const soundMessage = new Audio('media/sfx/msg.mp3');
+const soundWrong = new Audio('media/sfx/wrong.mp3');
+const soundRight = new Audio('media/sfx/cheer.mp3');
 
-
-
-const folder = "media/animal_classes/";
-const ext = ".jpg";
-
-const getFileName = function(index) {
-    return folder + animalClasses[index] + ext;
+const gameState = {
+    loadedAnimal:"",
 };
 
-const animalClassElement= function (index) {
+const animalClasses = {
+    amphibians: [],
+    arthropods: [],
+    birds: [],
+    fish: [],
+    mammals: [
+        "dog",
+        "cow",
+        "cat"
+    ],
+    reptiles: []
+};
+
+const animalClassesFolder = "media/animal_classes/";
+const ext = ".jpg";
+
+const getAnimalClassSubFolder = (animalClass) => animalClassesFolder + animalClass + "/";
+
+const getFileName = function(animalClass) {
+    return animalClassesFolder + animalClass + ext;
+};
+
+const animalClassElement = function(animalClass) {
     const ptagClass = 'animal-class-image-text';
     const animalImageClass = 'animal-class-image';
 
 
-    const fileName = getFileName(index);
-    let animalClassName = animalClasses[index].toUpperCase();
+    const fileName = getFileName(animalClass);
+    let animalClassName = animalClass.toUpperCase();
     const imgWrapper = document.createElement("div");
     imgWrapper.className = 'animal-image-wrapper';
 
     const imgTag = document.createElement("img");
     imgTag.className = animalImageClass;
     imgTag.src = fileName;
-    imgTag.onclick = function(){
-        loadAnimal(animalClassName.toLowerCase());
-    };
-
+    imgTag.addEventListener("click", ()=>loadAnimal(animalClassName.toLowerCase()));
     imgWrapper.appendChild(imgTag);
 
     const ptag = document.createElement("p");
@@ -51,18 +59,83 @@ const animalClassElement= function (index) {
 
 
 const loadAnimal = function(animalClass){
-    console.log(animalClass);
+    const imgId = "display-animal";
+    const view = document.getElementById("game-view");
+    const loadedAnimal = document.getElementById(imgId);
+    if (loadedAnimal)
+        view.removeChild(loadedAnimal);
+    const imgElement = document.createElement("img");
+    imgElement.id = imgId;
+    let randAnimal;
+    do {
+        randAnimal = getRandomAnimal(animalClass);
+    } while(randAnimal === gameState.loadedAnimal);
+    gameState.loadedAnimal = randAnimal;
+    imgElement.src = getAnimalClassSubFolder(animalClass) + randAnimal + ext;
+    view.appendChild(imgElement);
+    appendOptionsElements(createOptionsElements(animalClass));
 };
 
+const getRandomAnimal = (animalClass) => animalClasses[animalClass][randIndex(animalClass)];
+const randIndex = (animalClass) => Math.floor(Math.random() * (animalClasses[animalClass].length));
 
-const playAudio = function(){
+const playMsg = function(){
     soundMessage.play();
 };
 
+const hideClasses = () => $(".animal-image-wrapper").css('display', 'none');
+const unhideClasses = () => $(".animal-image-wrapper").css('display', '');
+
+
+// const createOptionsElements = function(animalClass){
+//     const ptagClass = 'options';
+//     // const divtagClass= 'option-wrapper';
+//
+//     const options = animalClasses[animalClass];
+//     const createdElements = [];
+//
+//     options.forEach(function(opt){
+//         const ptag = document.createElement("p");
+//         const textNode = document.createTextNode(opt.capitilize());
+//         ptag.appendChild(textNode);
+//         ptag.className = ptagClass;
+//         ptag.addEventListener("click", () => optionClickHandler(opt));
+//         createdElements.push(ptag);
+//     });
+//     return createdElements;
+// };
+const createOptionsElements = function(animalClass){
+    const btntagClass = 'option-button';
+    const options = animalClasses[animalClass];
+    const createdElements = [];
+
+    options.forEach(function(opt){
+        const btntag = document.createElement("button");
+        const textNode = document.createTextNode(opt.toUpperCase());
+        btntag.appendChild(textNode);
+        btntag.className = btntagClass;
+        btntag.addEventListener("click", () => optionClickHandler(opt));
+        createdElements.push(btntag);
+    });
+    return createdElements;
+};
+
+const appendOptionsElements = function(optionElements) {
+    const parent = document.getElementById('animal-class-wrapper');
+    hideClasses();
+    optionElements.forEach(function(ele){
+        parent.appendChild(ele);
+    });
+};
+
+
+const optionClickHandler = function(option){
+    console.log(option);
+};
 
 const appendAnimalClassImgs = function(){
     const animalClassContainer = document.getElementById("animal-class-wrapper");
-    for (let i = 0; i < animalClasses.length; i++) {
-        animalClassContainer.appendChild(animalClassElement(i));
+    for (let animalClass in animalClasses){
+        animalClassContainer.appendChild(animalClassElement(animalClass));
     }
 };
